5fdd458c543a52df64f7aa3b7669442437f83d65
bot-ti <tommilahtict@gmail.com>
2024-11-02 02:31:17 +0200

    Removed comment

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ async def run():
 
     try:
         # Start polling
-        # 'handle_as_tasks' runs updates in separate tasks, 'allowed_updates' specifies what kinds of updates
         await dispatcher_instance.start_polling(bot_instance, handle_as_tasks=True, allowed_updates=['message', 'callback_query'])
 
     except KeyboardInterrupt:
